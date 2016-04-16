a988c02400b5d7f01b4fef0708f2035ca2e892ad
hippomormor <cgenter@oncable.dk>
2016-04-16 15:49:17 +0200

    Initial commit (RMI)

--- src/java/RMI/DatabaseRMI.java
+++ src/java/RMI/DatabaseRMI.java
@@ -5,10 +5,93 @@
  */
 package RMI;
 
+import DAL.ComponentDTO;
+import DAL.ComponentGroupDTO;
+import DAL.LoanDTO;
+import DAL.StudentDTO;
+import java.rmi.RemoteException;
+import java.rmi.server.RMIClientSocketFactory;
+import java.rmi.server.RMIServerSocketFactory;
+import java.rmi.server.UnicastRemoteObject;
+
 /**
  *
  * @author hippomormor
  */
-public class DatabaseRMI {
+public class DatabaseRMI extends UnicastRemoteObject implements IDatabaseRMI {
+
+    public DatabaseRMI() throws RemoteException {
+    }
+
+    public DatabaseRMI(int port) throws RemoteException {
+        super(port);
+    }
+
+    @Override
+    public ComponentDTO getComponent(int componentId) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ComponentDTO[] getComponents() throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int updateComponent(ComponentDTO component) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ComponentGroupDTO getComponentGroup(int componentGroupId) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ComponentGroupDTO[] getComponentGroups() throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int updateComponentGroups(ComponentGroupDTO componentGroup) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int createLoan(LoanDTO loan) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public LoanDTO getLoan(int loanId) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public LoanDTO[] getLoans() throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int updateLoan(LoanDTO loan) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int deleteLoan(int loanId) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StudentDTO getStudent(String studentId) throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public StudentDTO[] getStudents() throws RemoteException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
     
 }
--- src/java/RMI/IDatabaseRMI.java
+++ src/java/RMI/IDatabaseRMI.java
@@ -5,10 +5,52 @@
  */
 package RMI;
 
+import DAL.ComponentDTO;
+import DAL.ComponentGroupDTO;
+import DAL.LoanDTO;
+import DAL.StudentDTO;
+
 /**
  *
  * @author hippomormor
  */
-public interface IDatabaseRMI {
+public interface IDatabaseRMI extends java.rmi.Remote {
+    
+    // Component
+    
+    ComponentDTO getComponent(int componentId) throws java.rmi.RemoteException;
+    
+    ComponentDTO[] getComponents() throws java.rmi.RemoteException;
+    
+    int updateComponent(ComponentDTO component) throws java.rmi.RemoteException;
+    
+    
+    // ComponentGroup
+
+    ComponentGroupDTO getComponentGroup(int componentGroupId) throws java.rmi.RemoteException;
+    
+    ComponentGroupDTO[] getComponentGroups() throws java.rmi.RemoteException;
+    
+    int updateComponentGroups(ComponentGroupDTO componentGroup) throws java.rmi.RemoteException;
+    
+    
+    // Loan
+ 
+    int createLoan(LoanDTO loan) throws java.rmi.RemoteException;
+
+    LoanDTO getLoan(int loanId) throws java.rmi.RemoteException;
+
+    LoanDTO[] getLoans() throws java.rmi.RemoteException;
+
+    int updateLoan(LoanDTO loan) throws java.rmi.RemoteException;
+
+    int deleteLoan(int loanId) throws java.rmi.RemoteException;
+    
+    
+    // Student
+
+    StudentDTO getStudent(String studentId) throws java.rmi.RemoteException;
+
+    StudentDTO[] getStudents() throws java.rmi.RemoteException;
     
 }
