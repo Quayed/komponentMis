b214e2eea1d18d35d17a6d78d016738fe88c66b1
hippomormor <cgenter@oncable.dk>
2016-04-17 14:59:28 +0200

    Server-side RMI-class

--- src/java/RMI/DatabaseRMI.java
+++ src/java/RMI/DatabaseRMI.java
@@ -5,12 +5,17 @@
  */
 package RMI;
 
+import DAL.ComponentDAO;
 import DAL.ComponentDTO;
+import DAL.ComponentGroupDAO;
 import DAL.ComponentGroupDTO;
+import DAL.LoanDAO;
 import DAL.LoanDTO;
+import DAL.StudentDAO;
 import DAL.StudentDTO;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
+import java.sql.Connection;
 
 /**
  *
@@ -18,81 +23,89 @@ import java.rmi.server.UnicastRemoteObject;
  */
 public class DatabaseRMI extends UnicastRemoteObject implements IDatabaseRMI {
 
+    private ComponentDAO component;
+    private ComponentGroupDAO componentgroup;
+    private LoanDAO loan;
+    private StudentDAO student;
     
-    
-    public DatabaseRMI() throws RemoteException {
-        
+    public DatabaseRMI(Connection conn) throws RemoteException {
+        init(conn);
     }
 
-    public DatabaseRMI(int port) throws RemoteException {
+    public DatabaseRMI(int port, Connection conn) throws RemoteException {
         super(port);
+        init(conn);     
+    }
+    
+    private void init(Connection conn) {
+       component = new ComponentDAO(conn);
+       componentgroup = new ComponentGroupDAO(conn);
+       loan = new LoanDAO(conn);
+       student = new StudentDAO(conn);
     }
 
     @Override
     public ComponentDTO getComponent(int componentId) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return component.getComponent(componentId);
     }
 
     @Override
     public ComponentDTO[] getComponents() throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return component.getComponents();
     }
 
     @Override
-    public int updateComponent(ComponentDTO component) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int updateComponent(ComponentDTO componentDTO) throws RemoteException {
+        return component.updateComponent(componentDTO);
     }
 
     @Override
     public ComponentGroupDTO getComponentGroup(int componentGroupId) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return componentgroup.getComponentGroup(componentGroupId);
     }
 
     @Override
     public ComponentGroupDTO[] getComponentGroups() throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return componentgroup.getComponentGroups();
     }
 
     @Override
-    public int updateComponentGroups(ComponentGroupDTO componentGroup) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int updateComponentGroups(ComponentGroupDTO componentGroupDTO) throws RemoteException {
+        return componentgroup.updateComponentGroups(componentGroupDTO);
     }
 
     @Override
-    public int createLoan(LoanDTO loan) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int createLoan(LoanDTO loanDTO) throws RemoteException {
+        return loan.createLoan(loanDTO);
     }
 
     @Override
     public LoanDTO getLoan(int loanId) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return loan.getLoan(loanId);
     }
 
     @Override
     public LoanDTO[] getLoans() throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return loan.getLoans();
     }
 
     @Override
-    public int updateLoan(LoanDTO loan) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public int updateLoan(LoanDTO loanDTO) throws RemoteException {
+        return loan.updateLoan(loanDTO);
     }
 
     @Override
     public int deleteLoan(int loanId) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return loan.deleteLoan(loanId);
     }
 
     @Override
     public StudentDTO getStudent(String studentId) throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return student.getStudent(studentId);
     }
 
     @Override
     public StudentDTO[] getStudents() throws RemoteException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-
-    
+        return student.getStudents();
+    } 
 }
