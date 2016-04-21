7b6f88fa40049e100e27dc98a0e4c5cc15d1041e
hippomormor <cgenter@oncable.dk>
2016-04-21 22:44:36 +0200

    Split TokenHandler in Client & Server Added support for multiple clients (added ID's)

---
+++ src/java/security/TokenHandlerServer.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package security;
+
+import com.sun.javafx.scene.control.skin.VirtualFlow;
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author hippomormor
+ */
+public class TokenHandlerServer {
+
+    private BigInteger randomToken;
+    private BigInteger publicToken;
+    private int clientCount = 0;
+    private ArrayList<BigInteger> keyList = new ArrayList<>();
+
+    // Hash credentials, reduce and generate random prime and token
+    public TokenHandlerServer(String user, String pass) {
+        generateRandom();
+        generateToken(new BigInteger(Integer.toString((user.hashCode() + pass.hashCode()) / 10000000 - 1)));
+    }
+
+    // Generate random 7-bit prime number
+    private BigInteger generateRandom() {
+        randomToken = BigInteger.probablePrime(7, new SecureRandom());
+        System.out.println("Random prime generated" + randomToken);
+        return randomToken;
+    }
+
+    // Generate token from random prime and hashed credentials: hashedCredentials^randomPrime (power)
+    private BigInteger generateToken(BigInteger credentials) {
+        publicToken = credentials.pow(Integer.parseInt(randomToken.toString()));
+        System.out.println("Token generated" + publicToken);
+        return publicToken;
+    }
+
+    // Generate key from recieved token and random prime: recievedToken^randomPrime (power)
+    public BigInteger generateKey(BigInteger token, int ID) {
+        BigInteger keyToken;
+        keyToken = token.pow(Integer.parseInt(randomToken.toString()));
+        keyList.set(ID, keyToken);
+        System.out.println("Key generated");
+        return keyList.get(ID);
+    }
+
+    // Check key with own key (should be equal)
+    public boolean checkKey(BigInteger key, int ID) {
+        if (key.equals(keyList.get(ID))) {
+            return true;
+        }
+        System.out.println("Key invalid");
+        return false;
+    }
+
+    // Get generated random prime
+    public BigInteger getRandomToken() {
+        return randomToken;
+    }
+
+    // Get generated token
+    public BigInteger getPublicToken() {
+        return publicToken;
+    }
+
+    // Get generated key
+    public BigInteger getKeyToken(int ID) {
+        return keyList.get(ID);
+    }
+
+    // Get new ID
+    public int getNewID() {
+        keyList.add(clientCount, BigInteger.ZERO);
+        return clientCount++;
+    }
+}
