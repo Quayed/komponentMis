4a00809a75effeb6cd52d4ceff2fc5205b9d4666
Mathias Sondrup <matsondrup@gmail.com>
2016-04-24 15:03:13 +0200

    added filters

---
+++ src/java/Rest/Filters/RestRequestFilter.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Rest.Filters;
+
+import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author mathias
+ */
+@Provider
+@PreMatching
+public class RestRequestFilter implements ContainerRequestFilter{
+
+    @Override
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+        // When HttpMethod comes as OPTIONS, just acknowledge that it accepts...
+        if ( requestContext.getRequest().getMethod().equals( "OPTIONS" ) ) {
+            System.out.println("Running");
+            // Just send a OK signal back to the browser
+            requestContext.abortWith( Response.status( Response.Status.OK ).build() );
+        }
+    }
+    
+}
---
+++ src/java/Rest/Filters/RestResponseFilter.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Rest.Filters;
+
+import java.io.IOException;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author mathias
+ */
+@Provider
+@PreMatching
+public class RestResponseFilter implements ContainerResponseFilter{
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        requestContext.getHeaders().add( "Access-Control-Allow-Origin", "*" );
+        requestContext.getHeaders().add( "Access-Control-Allow-Credentials", "true" );
+        requestContext.getHeaders().add( "Access-Control-Allow-Methods", "GET, POST, DELETE, PUT" );
+    }
+    
+}
